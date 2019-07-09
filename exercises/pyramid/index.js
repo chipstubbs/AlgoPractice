ae47e9dbe1de3ddce50f437a0251ddc9032fc949
Chip Stubbs <cstubbs@advisorsacademy.com>
2019-07-09 13:35:44 -0400

    finished pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 1) {
+    const space = ' ';
+    const pound = '#';
+    
+    // Base Case
+    if (n === row) {
+        console.log(pound.repeat( (n*2)-1) );
+        return;
+    }
+
+    // Log each row 
+    if (n > row) {
+        console.log( space.repeat(n - row) + pound.repeat( (row * 2) - 1 ) + space.repeat( n - row ) );
+    }
+
+    // Loop recursively until rows = n
+    pyramid(n, row + 1);
+}
 
 module.exports = pyramid;
+
+// My Solutions
+// function pyramid(n) {
+//     if (n < 0) { n = n * -1; }
+//     let pyr = '';
+//     for (i=1; i <= n; i++) {
+//       const space = ' ';
+//       const pound = '#';
+//       if(i === n) {
+//         pyr = pound.repeat((n*2)-1);
+//       } else {
+//         pyr = (space.repeat(n-i) + pound.repeat((i*2)-1) + space.repeat(n-i) );
+//       }
+//       console.log(pyr);
+//     }
+// }
+
